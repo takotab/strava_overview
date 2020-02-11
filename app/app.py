5491d0eaeab8e6d2086929a32ebc943be935b520
tako_desktop <takotabak@gmail.com>
2020-02-11 08:32:23 +0100

    more tests w streamlit

--- app/app.py
+++ app/app.py
@@ -6,13 +6,17 @@ import pandas as pd
 def main():
     st.title("Motion Review")
     st.write("Een overview van jou trainingsweek")
-    client_id, secret = open("client.secret").read().strip().split(",")
-    h = Handel(client_id, secret)
-    h.start()
-    st.write(f"Welcome {h.athlete.firstname} {h.athlete.lastname}")
-    acts = h.get_activities()
-    # fs = h.parse_activitys(acts)
-    st.write(acts)
+    email = st.text_input("Email adres")
+    psw = st.text_input("Wachtwoord", type="password")
+    if psw:
+        st.balloons()
+        client_id, secret = open("client.secret").read().strip().split(",")
+        h = Handel(client_id, secret)
+        h.start()
+        st.write(f"Welcome {h.athlete.firstname} {h.athlete.lastname}")
+        acts = h.get_activities()
+        # fs = h.parse_activitys(acts)
+        st.write(acts)
 
 
 if __name__ == "__main__":
