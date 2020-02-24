d2a27fbfe9e65c3234f49a73dcdb40ce114d2f3c
Tako Tabak (ryzen) <TakoTabak@gmail.com>
2020-02-24 15:20:32 +0100

    on the right port :finaly:

--- docker_start.sh
+++ docker_start.sh
@@ -3,8 +3,9 @@ docker rm $(docker ps -a -q)
 # docker rmi $(docker images -q)
 docker build -t str_over .
 # docker run -d --name mycontainer -p 80:80 myimage
-docker run -it -p 80:8080 -e PORT="8080" \
-            -e WORKERS_PER_CORE="0.1"\
-            -e BIND="0.0.0.0:8080" str_over
+docker run -it -p 8080:8080 -e PORT="8080" \
+            -e WORKERS_PER_CORE="0.1" str_over
+            # -e BIND="0.0.0.0:8080"\
+             
             # -v $PWD/shared:app/shared 
             
