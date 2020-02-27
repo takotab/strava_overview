446d1404333e7d3d59802981c1bdcd0f294e4ae7
Tako Tabak (ryzen) <TakoTabak@gmail.com>
2020-02-27 16:03:58 +0100

    git_add

--- strava_overview/imports.py
+++ strava_overview/imports.py
@@ -7,4 +7,24 @@ import sys
 from fastcore.utils import *
 import time
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
+host = os.getenv("host")
+client_id = os.getenv("client_id")
+secret = os.getenv("secret")
+
+from git import Repo
+from nbdev.export import Config as nb_Config
+from nbdev.export import *
+
+def git_add(fname, commit_msg='.'):
+    repo = Repo(nb_Config().nbs_path.parent)
+    notebook2script(fname)
+    nb = read_nb(fname)
+    default = find_default_export(nb['cells'])
+    py = [os.path.join(nb_Config().lib_path,*default.split('.'))+'.py',
+          os.path.join(nb_Config().nbs_path,fname)
+         ]
+    repo.index.add(py)
+    repo.index.commit(commit_msg)
+    return py
